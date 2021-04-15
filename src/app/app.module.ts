5ef3c40faeb9c22de85e7788020a45e29efb5c6e
raymondaksu <raymondaksu@users.noreply.github.com>
2021-04-15 20:36:17 +0300

    Navigation and routing basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,28 @@ import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpaces } from './shared/convert-to-spaces.pip';
 import { StarComponent } from './shared/star.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductDetailGuard } from './products/product-detail.guard';
 
 @NgModule({
   declarations: [
     AppComponent,
+    WelcomeComponent,
     ProductListComponent,
     ConvertToSpaces,
     StarComponent,
+    ProductDetailComponent,
   ],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot([
+    { path: 'products', component: ProductListComponent },
+    { path: 'products/:id', canActivate: [ProductDetailGuard], component: ProductDetailComponent },
+    { path: 'welcome', component: WelcomeComponent },
+    { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
+  ])
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
